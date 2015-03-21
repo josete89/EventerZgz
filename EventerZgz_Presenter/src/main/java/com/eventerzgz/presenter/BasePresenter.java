030810cc72f88bcc90a9723dece105a1ad5c1764
josete_89 <josete_zgz_89@hotmail.com>
2015-03-21 18:39:15 +0100

    Add event in calendar

--- EventerZgz_Presenter/src/main/java/com/eventerzgz/presenter/BasePresenter.java
+++ EventerZgz_Presenter/src/main/java/com/eventerzgz/presenter/BasePresenter.java
@@ -1,5 +1,13 @@
 package com.eventerzgz.presenter;
 
+import java.util.Calendar;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.CalendarContract;
+import com.eventerzgz.model.event.Event;
 import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
@@ -18,6 +26,35 @@ public abstract class BasePresenter
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(subscriber);
 
+    }
+
+
+
+
+    public void addCalendarEvent(Event event,Context ctx)
+    {
+
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.setTime(event.getdStartDate());
+        Calendar endTime = Calendar.getInstance();
+        endTime.setTime(event.getdEndDate());
+
+        String title = event.getsTitle();
+        String description = event.getsDescription();
+        String location = event.getSubEventList() != null && event.getSubEventList().size() > 0 ? event.getSubEventList().get(0).getObjPlace().getsAddress():"";
+        String email = event.getSubEventList() != null && event.getSubEventList().size() > 0 ? event.getSubEventList().get(0).getObjPlace().getsMail():"";
+
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                .putExtra(CalendarContract.Events.TITLE, title)
+                .putExtra(CalendarContract.Events.DESCRIPTION, description)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_FREE)
+                .putExtra(Intent.EXTRA_EMAIL, email);
+
+        ctx.startActivity(intent);
     }
 
 }
